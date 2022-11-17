3395f4b1869ff988e41e20d28c57b097686145fd
Pankeeva Olga <0lga.pankeeva@mail.ru>
2022-11-18 00:23:53 +0400

    correct

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   
-  return arr1.lenght === arr2.lenght && arr1.every((n, i) => n === arr2[i]); 
-  
+  return arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]); 
+
 }
 
 function advancedFilter(arr) {
