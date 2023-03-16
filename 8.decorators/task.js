be2876ae04e947d6fbd3a9d31616680c7063de1d
Olga Pankeeva <0lga.pankeeva@mail.ru>
2023-03-16 15:17:06 +0400

    add decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,42 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+ let cache = [];
+
+   function wrapper(...args) {
+     const hash = args.join(",");
+     let index = cache.findIndex(item => item.hash == hash);
+     if (index !== -1) {
+       console.log("Из кэша: " + cache[index].value);
+       return "Из кэша: " + cache[index].value;
+     }
+     let result = func(...args);
+     cache.push({
+       hash: hash,
+       value: result,
+     });
+
+     if (cache.length > 5) {
+       cache.shift();
+     }
+     console.log("Вычисляем: " + result);
+     return "Вычисляем: " + result;
+   }
+   return wrapper;
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  var debounceDecoratorNewCalls = 0;
+    let timeoutId = null;
+
+    function wrapper(...args) {
+      if (timeoutId === null) {
+        func(...args);
+      }
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        timeoutId = null;
+      }, delay);
+      wrapper.count++;
+      wrapper.allCount = debounceDecoratorNewCalls++;
+    }
+    return wrapper;
 }
